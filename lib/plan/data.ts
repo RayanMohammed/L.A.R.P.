727c3579764881d6b44ae6c87adac2d3136f75c8
Zahir Ali <zahir.ali986@gmail.com>
2026-05-09 16:37:45 -0700

    fixed tse link

--- lib/plan/data.ts
+++ lib/plan/data.ts
@@ -1,18 +1,13 @@
-import archetypes from "./data/jobArchetypes.json";
-import resources from "./data/campusResources.json";
+import type { CampusResource } from "./types";
+import { RESOURCES, getResource } from "./catalog";
 import { getTopResourceIds } from "./embeddings";
-import type { CampusResource, JobArchetype } from "./types";
 
-export const ARCHETYPES: JobArchetype[] = archetypes as JobArchetype[];
-export const RESOURCES: CampusResource[] = resources as CampusResource[];
-
-export function getArchetype(id: string): JobArchetype | undefined {
-  return ARCHETYPES.find((a) => a.id === id);
-}
-
-export function getResource(id: string): CampusResource | undefined {
-  return RESOURCES.find((r) => r.id === id);
-}
+export {
+  ARCHETYPES,
+  RESOURCES,
+  getArchetype,
+  getResource,
+} from "./catalog";
 
 /**
  * Semantic resource retrieval.
